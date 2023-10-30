8e66f9fa15ad37efac4b3cf727c59d2257c13e0e
henrique <henriquevs98@gmail.com>
2023-10-30 00:33:51 +0000

    cleaning

--- app/main.py
+++ app/main.py
@@ -1,20 +1,6 @@
 from app.libs import nhtsa
-import logging
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-# Create a StreamHandler to print log messages to the console
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-# Create a Formatter to format log messages
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-# Add the Formatter to the StreamHandler
-console_handler.setFormatter(formatter)
-# Add the StreamHandler to the logger
-logging.getLogger().addHandler(console_handler)
-
 
 # Get complains as a pandas df
 df_all_complaints = nhtsa.get_all_complaints()
-# Save the dataframe as a csv file with a semicolon delimiter and no quotes
-df_all_complaints.to_csv('app/files/nhtsa/extracted/complaints.csv', index=False, sep=';')
+# Save the dataframe as a csv 
+nhtsa.save_to_csv(df_all_complaints, 'app/files/nhtsa/extracted/complaints.csv')
