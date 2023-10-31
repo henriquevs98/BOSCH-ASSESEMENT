cacf4039a6492344bf9c5337aa122afbd3ae76f6
henrique <henriquevs98@gmail.com>
2023-10-31 03:08:50 +0000

    added lib for data extraction functions

---
+++ app/libs/extractor.py
@@ -0,0 +1,18 @@
+import os
+import requests
+import logging
+from app import logger
+
+# Load logging settings
+logger.logger()
+
+
+def extract_response(url):
+    try:
+        # Send an HTTP GET request to the API endpoint and store the response
+        response = requests.get(url, stream=False)
+        
+        return response
+
+    except Exception as e:
+        logging.error(f'Process {os.getpid()}: An error occurred when using nhtsa_extract(): {e}')
