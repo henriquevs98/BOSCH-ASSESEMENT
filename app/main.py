891a719ca3afbfff8aeb3e61b88ec997f8359ec8
henrique <henriquevs98@gmail.com>
2023-10-31 04:04:33 +0000

    updated with the latest functionalities

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from app.libs import nhtsa
+from app import logger
+from app.libs import complaints, fueleco, stations, loader
 
-# Get complains as a pandas df
-df_all_complaints = nhtsa.get_all_complaints()
+# Load logging settings
+logger.logger()
+
+df_all_complaints = complaints.get_all_complaints()
 # Save the dataframe as a csv 
-nhtsa.save_to_csv(df_all_complaints, 'app/files/nhtsa/extracted/complaints.csv')
+loader.df_to_csv(df_all_complaints, 'app/files/complaints/extracted/complaints.csv')
+
+# Get fuel economy as a pandas df
+df_all_stations = stations.get_stations()
+# Save the dataframe as a csv 
+loader.df_to_csv(df_all_stations, 'app/files/stations/extracted/stations.csv')
+
+# Get fuel economy as a pandas df
+df_all_fueleco = fueleco.get_fueleco()
+# Save the dataframe as a csv 
+loader.df_to_csv(df_all_fueleco, 'app/files/fueleco/extracted/fueleco.csv')
