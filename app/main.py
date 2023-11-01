cf4d7fd6863a6607174f34d371724bcfba93477a
henrique <henriquevs98@gmail.com>
2023-11-01 17:48:30 +0000

    updated with the latest functionalities

--- app/main.py
+++ app/main.py
@@ -1,59 +1,90 @@
 from app import logger
-from app.libs import complaints, fueleco, stations, loader, cleaner, transformer
-from app.data import stations_mapping
+from app.libs import loader, cleaner, transformer, extractor
+from app.data import stations_mapping, complaints_mapping
 
 # Load logging settings
 logger.logger()
 
-# Get Vehicle Complaints Information dataset as a pandas df
-df_all_complaints = complaints.get_all_complaints()
-# Save the dataframe as a csv 
-loader.df_to_csv(df_all_complaints, 'app/data/complaints/extracted/complaints.csv')
-#Profile df in order to analyze what to clean
-cleaner.profile_df(df_all_complaints, 'Vehicle Fuel Economy Information', 'app/data/fueleco/profile_fueleco.html')
+def extraction_complaints():
+    # Get Vehicle Complaints Information dataset as a pandas df
+    df_all_complaints = extractor.get_all_complaints()
+    # Save the dataframe as a csv 
+    loader.df_to_csv(df_all_complaints, 'app/data/complaints/extracted/complaints.csv')
 
 
-# Get Alternative Fuel Stations dataset as a pandas df
-df_all_stations = stations.get_stations()
-# Save the extracted dataframe as a csv 
-loader.df_to_csv(df_all_stations, 'app/data/stations/extracted/stations.csv')
-import pandas as pd
-df_all_stations = pd.read_csv('app/data/stations/extracted/stations.csv', sep=';')
-
-# Separate column names with _ and lowercase
-df_all_stations = cleaner.fix_columns(df_all_stations)
-# Remove deprecated columns (as indicated in documentation)
-df_all_stations = cleaner.rm_columns(df_all_stations, stations_mapping.deprecated_columns)
-# Remove columns that arent too revelant
-df_all_stations = cleaner.rm_columns(df_all_stations, stations_mapping.trash_columns)
-
-# Map status_code values according to documentation to new status column and drop column used
-df_all_stations = transformer.map_column_values(df_all_stations, 'status_code', 'status', stations_mapping.status_map)
-# Map owner_type_code values according to documentation to new owner_type column and drop column used
-df_all_stations = transformer.map_column_values(df_all_stations, 'owner_type_code', 'owner_type', stations_mapping.owner_type_map)
-# Map fuel_type_coded values according to documentation to new fuel_type column and drop column used
-df_all_stations = transformer.map_column_values(df_all_stations, 'fuel_type_code', 'fuel_type', stations_mapping.fuel_type_map)
-# Map facility_type values according to documentation to new facility column and drop column used
-df_all_stations = transformer.map_column_values(df_all_stations, 'facility_type', 'facility', stations_mapping.facility_map)
-# Map maximum_vehicle_class values according to documentation to new maximum_class column and drop column used
-df_all_stations = transformer.map_column_values(df_all_stations, 'maximum_vehicle_class', 'maximum_class', stations_mapping.maximum_vehicle_map)
-
-# Create adress column cleaning street_adress, city, state, zip-plus4 and drop columns used
-df_all_stations = transformer.stations_create_adress(df_all_stations)
-# Create a GIS readable point location column named adress using lat and long and drop columns used
-df_all_stations = transformer.stations_create_point(df_all_stations)
-# Drop rows where station_name is Null
-df_all_stations = df_all_stations.dropna(subset=['station_name'])
-# Clean possible null values in str columns
-df_all_stations = cleaner.replace_null_values(df_all_stations, ['access_code', 'station_address'], 'Unknown')
-# Convert updated_at column to datetime dtype
-df_all_stations = transformer.convert_to_datetime(df_all_stations, 'updated_at')
-
-# Save the processed dataframe as a csv 
-loader.df_to_csv(df_all_stations, 'app/data/stations/processed/stations.csv')
+def extraction_stations():
+    # Get Alternative Fuel Stations dataset as a pandas df
+    df_stations = extractor.get_stations()
+    # Save the extracted dataframe as a csv 
+    loader.df_to_csv(df_stations, 'app/data/stations/extracted/stations.csv')
 
 
-# Get Vehicle Fuel Economy Information dataset as a pandas df
-df_all_fueleco = fueleco.get_fueleco()
-# Save the dataframe as a csv 
-loader.df_to_csv(df_all_fueleco, 'app/data/fueleco/extracted/fueleco.csv')
+def extraction_fuel():
+    # Get Vehicle Fuel Economy Information dataset as a pandas df
+    df_all_fuel = extractor.get_fuel()
+    # Save the dataframe as a csv 
+    loader.df_to_csv(df_all_fuel, 'app/data/fuel/extracted/fuel.csv')
+
+
+def transformation_complaints():
+    # Read the extracted csv
+    df_complaints = loader.csv_to_df('app/data/complaints/extracted/complaints.csv')
+
+    # Remove columns that arent too revelant
+    df_complaints = cleaner.rm_columns(df_complaints, complaints_mapping.trash_columns)
+    # Fix dates
+    df_complaints = transformer.fix_date_columns(df_complaints, complaints_mapping.fix_date)
+    # Fix word capitalization on 
+    df_complaints = transformer.fix_capitalize_words(df_complaints, complaints_mapping.fix_capitalize)
+    # Create a complaintYear column
+    df_complaints = transformer.create_year_column(df_complaints, 'dateComplaintFiled',  'complaintYear')
+    # Convert columns types to date
+    df_complaints = transformer.convert_columns_to_datetime(df_complaints, complaints_mapping.convert_to_datetime)
+    #Convert columns types to int
+    df_complaints = transformer.convert_columns_to_int(df_complaints, complaints_mapping.convert_to_int)
+    # Reorder columns in df
+    df_complaints = transformer.reorder_columns(df_complaints, )
+    # Create a column with a unique ID in  the first column index
+    df_complaints = transformer.create_id_column(df_complaints)
+
+    # Save the processed dataframe as a csv 
+    loader.df_to_csv(df_complaints, 'app/data/complaints/processed/complaints.csv')
+
+
+def transformation_stations():
+    # Read the extracted csv
+    df_stations = loader.csv_to_df('app/data/stations/extracted/stations.csv')
+
+    # Separate column names with _ and lowercase
+    df_stations = cleaner.fix_columns(df_stations)
+    # Remove deprecated columns (as indicated in documentation)
+    df_stations = cleaner.rm_columns(df_stations, stations_mapping.deprecated_columns)
+    # Remove columns that arent too revelant
+    df_stations = cleaner.rm_columns(df_stations, stations_mapping.trash_columns)
+    # Map status_code values according to documentation to new status column and drop column used
+    df_stations = transformer.map_column_values(df_stations, 'status_code', 'status', stations_mapping.status_map)
+    # Map owner_type_code values according to documentation to new owner_type column and drop column used
+    df_stations = transformer.map_column_values(df_stations, 'owner_type_code', 'owner_type', stations_mapping.owner_type_map)
+    # Map fuel_type_coded values according to documentation to new fuel_type column and drop column used
+    df_stations = transformer.map_column_values(df_stations, 'fuel_type_code', 'fuel_type', stations_mapping.fuel_type_map)
+    # Map facility_type values according to documentation to new facility column and drop column used
+    df_stations = transformer.map_column_values(df_stations, 'facility_type', 'facility', stations_mapping.facility_map)
+    # Map maximum_vehicle_class values according to documentation to new maximum_class column and drop column used
+    df_stations = transformer.map_column_values(df_stations, 'maximum_vehicle_class', 'maximum_class', stations_mapping.maximum_vehicle_map)
+    # Create adress column cleaning street_adress, city, state, zip-plus4 and drop columns used
+    df_stations = transformer.stations_create_adress(df_stations)
+    # Create a GIS readable point location column named adress using lat and long and drop columns used
+    df_stations = transformer.stations_create_point(df_stations)
+    # Drop rows where station_name is Null
+    df_stations = df_stations.dropna(subset=['station_name'])
+    # Clean possible null values in str columns
+    df_stations = cleaner.replace_null_values(df_stations, stations_mapping.replace_null_col_value, 'Unknown')
+    # Convert updated_at column to datetime dtype
+    df_stations = transformer.convert_columns_to_datetime(df_stations, stations_mapping.convert_to_datetime)
+    # Reorder columns in df
+    df_stations = transformer.reorder_columns(df_stations, stations_mapping.reorder_columns)
+    # Create a column with a unique ID in  the first column index
+    df_stations = transformer.create_id_column(df_stations)
+
+    # Save the processed dataframe as a csv 
+    loader.df_to_csv(df_stations, 'app/data/stations/processed/stations.csv')
